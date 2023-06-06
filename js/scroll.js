c1b5f6f3993f3e2f671909c9a2d922513f96083e
jeedix <106433450+jeedix@users.noreply.github.com>
2023-06-06 19:52:09 +0500

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,4 @@
 (function () {
-    const header = document.querySelector('.header');
-    window.onscroll = () => {
-        if (window.pageYOffset > 50) {
-            header.classList.add('header_active');
-        } else {
-            header.classList.remove('header_active');
-        }
-    };
-}());
-(function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
     const menuCloseItem = document.querySelector('.header__nav-close');
