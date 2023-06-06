75e1bdf6c3c5270280dcfbac2d427df26559bc56
jeedix <106433450+jeedix@users.noreply.github.com>
2023-06-06 19:45:59 +0500

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,23 +1,21 @@
-!function(){
-    var e=document.querySelector(".header");
-    window.onscroll=function(){window.pageYOffset>50?e.classList.add("header_active"):
-    e.classList.remove("header_active")}}(),function(){
-    var e=document.querySelector(".burger"),t=document.querySelector(".header__nav"),
-    n=document.querySelector(".header__nav-close"),r=document.querySelectorAll(".header__link");
-    if(e.addEventListener("click",function()
-    {t.classList.add("header__nav_active")}),
-    n.addEventListener("click",function()
-    {t.classList.remove("header__nav_active")}),window.innerWidth<=767)
-    for
-        (var c=0;c<r.length;c+=1)r[c].addEventListener("click",function()
-    {t.classList.remove("header__nav_active")})}(),
-    document.querySelectorAll(".js-scroll").forEach(function(e)
-    {e.addEventListener("click",function()
-    {var e,t,n,r,c,i,o=this.getAttribute("href");
-    e=o,t=1e3,n=document.querySelector(".header").clientHeight,
-    r=document.querySelector(e).getBoundingClientRect().
-    top-n,c=window.pageYOffset,i=null,
-    requestAnimationFrame(function e(n){null===i&&(i=n);
-    var o,a,d,l=n-i,u=(o=l,a=c,d=r,(o/=t/2)<1?d/2*o*o+a:-d/2*(--o*(o-2)-1)+a);window.scrollTo(0,u),
-    l<t&&requestAnimationFrame(e)
-})})});
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active');
+        }
+    };
+}());
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+    })
+}());
